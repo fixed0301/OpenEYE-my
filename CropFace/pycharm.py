6bf052a145ed4854062e1ac236da80cf7f201605
fixed0301 <fixeddoor0301@gmail.com>
2023-07-29 04:11:37 +0900

    asdf

--- CropFace/pycharm.py
+++ CropFace/pycharm.py
@@ -1,77 +1,59 @@
 import cv2
-import mediapipe as mp
-from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
+from retinaface import RetinaFace
 
-#모듈화됐으나 detect 낮음
-
-mp_face = mp.solutions.face_detection.FaceDetection(
-    model_selection=1,
-    min_detection_confidence=0.5
-)
-#E:/2023/2023_1_1/comp/CropFace/multiperson.jpg
 class Face:
     def __init__(self, img):
         self.img = img
-        self.results = mp_face.process(img)
-
-    def cropface(self, image_input, draw=False):
-        if self.results.detections is None:
-            return ['N']
+        self.faces = RetinaFace.extract_faces(img_path=self.img, align = False)
+        self.detect_faces = RetinaFace.detect_faces(img)
+        self.sorted_people = [] #from left
+    def detect(self, show_results = True):
+        if self.detect_faces is None:
+            return []
         else:
-            people = []
-            for detection in self.results.detections:
-                location = detection.location_data
-                relative_bounding_box = location.relative_bounding_box
-                rect_start = _normalized_to_pixel_coordinates(
-                  relative_bounding_box.xmin, relative_bounding_box.ymin, image_cols,
-                  image_rows)
-                rect_end = _normalized_to_pixel_coordinates(
-                  relative_bounding_box.xmin + relative_bounding_box.width,
-                  relative_bounding_box.ymin + relative_bounding_box.height, image_cols,
-                  image_rows)
-                xleft, ytop = rect_start
-                xright, ybot = rect_end
-
-                if draw:
-                    color = (255, 0, 0)
-                    thickness = 2
-                    cv2.rectangle(image_input, rect_start, rect_end, color, thickness)
-                crop_img = image_input[ytop: ybot, xleft: xright]
-                people.append([crop_img, xleft])
-            return people
-
-    def sort_from_left(self, people):
-
-        return people
-    def num_people(self, people):
-        return len(people)
-
-    def imshow(self, img):
-        cv2.imshow('show', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            tmp = []
+            for faceNum in self.detect_faces.keys():
+                identity = self.detect_faces[f'{faceNum}']
+                facial_area = identity["facial_area"]
+                facial_img = self.img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
+                tmp.append([faceNum, facial_img, facial_area[0]])
 
 
-#E:/2023/2023_1_1/comp/CropFace/multiperson.jpg
+                if show_results == True:
+                    cv2.rectangle(self.img, (facial_area[0], facial_area[1])
+                                  , (facial_area[2], facial_area[3]), (255, 255, 255), 1)
+                    cv2.putText(self.img, f'{faceNum}', (facial_area[0], facial_area[1]),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+
+            tmp = sorted(tmp, key=lambda x: x[2])
+            self.sorted_people = list(map(lambda x: x[:2], tmp))
+
+            if show_results == True:
+                return self.img
+
+        return self.sorted_people #[faceNum, facial_img]
+
+    def num_people(self):
+        return len(self.sorted_people)
+    def save_img(self, faceNum, facial_img):
+        cv2.imwrite('/7multi/'+f'{faceNum}'+'.jpg', facial_img)
+
+
+
 img = cv2.imread("E:/im/7multi.jpg")
-cv2.resize(img, (1000, 1260))
-image_rows, image_cols, _ = img.shape
 
 face = Face(img)
-people = face.cropface(img, draw=True)
+sorted_people = face.detect(show_results=False)
+for person in sorted_people:
+    faceNum, facial_img = person[0], person[1]
+    #face.save_img(faceNum, facial_img)
+    facial_img.classify() #classify를 여기에 넣자
 
-num_people = face.num_people(people)
-print(num_people)
+cv2.imshow('asdf', face.detect(show_results=True))
 
-cropped1 = people[1][0]
-idx = people[0][1]
-
-
-cv2.imshow('show', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-#if cropped != 'N':
-#    face.imshow('asdf', cropped)
 
 
 
--- CropFace/retina.py
+++ CropFace/retina.py
@@ -10,18 +10,17 @@ for faceNum in faces.keys():
     landmarks = identity["landmarks"]
 
     # highlight facial area
-    cv2.rectangle(img, (facial_area[2], facial_area[3])
-                  , (facial_area[0], facial_area[1]), (255, 255, 255), 1)
+    cv2.rectangle(img, (facial_area[0], facial_area[1])
+                  , (facial_area[2], facial_area[3]), (255, 255, 255), 1)
 
 
-# extract facial area
+
 
 # facial_img = img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
 
 cv2.imshow('asdf', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-# highlight the landmarks
 
 # cv2.circle(img, tuple(landmarks["left_eye"]), 1, (0, 0, 255), -1)
 # cv2.circle(img, tuple(landmarks["right_eye"]), 1, (0, 0, 255), -1)
