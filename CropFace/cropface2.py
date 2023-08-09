e1f6d2672dea1ad5723002d58da40b0e99b2cbe0
fixed0301 <129080411+fixed0301@users.noreply.github.com>
2023-08-09 21:17:40 +0900

    Delete CropFace directory

--- CropFace/cropface2.py
+++
@@ -1,7 +0,0 @@
-import cv2
-from retinaface import RetinaFace
-faces = RetinaFace.extract_faces(img_path = "multiperson.jpg", align = False)
-cnt = 0
-for face in faces:
-    cv2.imwrite(f'{cnt}.jpg', cv2.cvtColor(face, cv2.COLOR_BGR2RGB))
-    cnt += 1
